f194a84dad1c17dc49cf7cab70884d21eb38e59e
im-devendhar <devendharyadav781@gmail.com>
2025-09-10 13:13:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,46 @@
+
 # Specify the AWS provider and region
 provider "aws" {
   region = "us-east-1"
 }
 
-# Create a custom VPC with a CIDR block
-resource "aws_vpc" "custom_vpc" {
+# Create a POC VPC with a CIDR block
+resource "aws_vpc" "POC_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Create a subnet within the custom VPC
-resource "aws_subnet" "custom_subnet" {
-  vpc_id     = aws_vpc.custom_vpc.id
+# Create a subnet within the POC VPC
+resource "aws_subnet" "POC_subnet" {
+  vpc_id     = aws_vpc.POC_vpc.id
   cidr_block = "10.0.1.0/24"
 }
 
 # Create an Internet Gateway and attach it to the VPC
-resource "aws_internet_gateway" "custom_igw" {
-  vpc_id = aws_vpc.custom_vpc.id
+resource "aws_internet_gateway" "POC_igw" {
+  vpc_id = aws_vpc.POC_vpc.id
 }
 
 # Create a route table for the VPC with a route to the Internet
-resource "aws_route_table" "custom_rt" {
-  vpc_id = aws_vpc.custom_vpc.id
+resource "aws_route_table" "POC_rt" {
+  vpc_id = aws_vpc.POC_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.custom_igw.id
+    gateway_id = aws_internet_gateway.POC_igw.id
   }
 }
 
 # Associate the route table with the subnet
-resource "aws_route_table_association" "custom_rta" {
-  subnet_id      = aws_subnet.custom_subnet.id
-  route_table_id = aws_route_table.custom_rt.id
+resource "aws_route_table_association" "POC_rta" {
+  subnet_id      = aws_subnet.POC_subnet.id
+  route_table_id = aws_route_table.POC_rt.id
 }
 
 # Create a security group that allows SSH access
-resource "aws_security_group" "custom_sg" {
-  name        = "custom_sg"
+resource "aws_security_group" "POC_sg" {
+  name        = "POC_sg"
   description = "Allow SSH"
-  vpc_id      = aws_vpc.custom_vpc.id
+  vpc_id      = aws_vpc.POC_vpc.id
 
   ingress {
     from_port   = 22
@@ -56,16 +57,15 @@ resource "aws_security_group" "custom_sg" {
   }
 }
 
-# Launch an EC2 instance in the custom subnet with the security group
-resource "aws_instance" "custom_instance" {
+# Launch an EC2 instance in the POC subnet with the security group
+resource "aws_instance" "POC_instance" {
   ami                    = "resolve:ssm:/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
   instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.custom_subnet.id
-  vpc_security_group_ids = [aws_security_group.custom_sg.id]
+  subnet_id              = aws_subnet.POC_subnet.id
+  vpc_security_group_ids = [aws_security_group.POC_sg.id]
 }
 
 # Create an S3 bucket
-resource "aws_s3_bucket" "custom_bucket" {
-  bucket = "devendhar-custom-bucket"
+resource "aws_s3_bucket" "POC_bucket" {
+  bucket = "devendhar-POC-bucket"
 }
-
